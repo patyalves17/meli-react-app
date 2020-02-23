5e38596a03ddd2b5632562bea62a479959b79ac9
Paty Alves <patriciaalves10@gmail.com>
2020-02-23 18:23:55 -0300

    styling the navbar component

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './Toolbar.scss';
 
 const Toolbar = props => {
-  return <nav className='Toolbar'></nav>;
+  return (
+    <nav className='Toolbar'>
+      <input type='text' className='Toolbar__input' />
+      <button className='Toolbar__button' type='button'>
+        <img src={require('./../assets/ic_Search.png')} />
+      </button>
+    </nav>
+  );
 };
 
 export default Toolbar;
